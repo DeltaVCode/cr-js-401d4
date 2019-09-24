6019642085a80418b20ef03757996d0c82832baa
Keith Dahlby <keith@newbo.co>
2019-09-24 11:10:36 -0500

    TDD POST + GET all

--- curriculum/class-07/demo/api-server/lib/server.js
+++ curriculum/class-07/demo/api-server/lib/server.js
@@ -4,6 +4,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello, world!'));
+
+let db = [];
+app.get('/categories', (req, res) => {
+  res.send(db);
+});
+
+app.post('/categories', (req, res) => {
+  let newRecord = {
+    ...req.body,
+    id: Math.random(),
+  }
+  db.push(newRecord);
+  res.status(201).send(newRecord);
+});
+
 module.exports = {
   server: app,
   start: port => {
