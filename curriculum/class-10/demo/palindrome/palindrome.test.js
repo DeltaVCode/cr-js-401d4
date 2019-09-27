4da0b66cc938a8fed3b2b01bd4b607b744978e09
Keith Dahlby <keith@newbo.co>
2019-09-27 10:48:24 -0500

    Add more tests, more LL functionality

--- curriculum/class-10/demo/palindrome/palindrome.test.js
+++ curriculum/class-10/demo/palindrome/palindrome.test.js
@@ -1,27 +1,54 @@
 function isPalindrome(list) {
-  return false;
+  return true;
 }
 
 describe('isPalindrome', () => {
-  it('returns false for empty list', () => {
+  it('returns true for empty list', () => {
     let list = new LinkedList();
     expect(list.toArray()).toEqual([]);
 
     let res = isPalindrome(list);
 
-    expect(res).toBe(false);
+    expect(res).toBe(true);
+  });
+
+  it('returns true for singleton', () => {
+    let list = new LinkedList(1);
+    expect(list.toArray()).toEqual([1]);
+
+    let res = isPalindrome(list);
+
+    expect(res).toBe(true);
+  });
+
+  it('returns true for doubleton palindrome', () => {
+    let list = new LinkedList(1, 1);
+    expect(list.toArray()).toEqual([1,1]);
+
+    let res = isPalindrome(list);
+
+    expect(res).toBe(true);
   });
 });
 
 class LinkedList {
-  constructor() {
+  constructor(...values) {
     this.head = null;
+
+    for(let i = 0; i < values.length; i++) {
+      let oldHead = this.head;
+      this.head = new Node(values[i]);
+      this.head.next = oldHead;
+    }
   }
 
   toArray() {
     let res = [];
     let current = this.head;
-    while(current) res.push(current.value);
+    while(current){
+      res.push(current.value);
+      current = current.next;
+    }
     return res;
   }
 }
