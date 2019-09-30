588732b2bb850d25dfef902d6ba8b9dd101e84e8
Keith Dahlby <keith@newbo.co>
2019-09-30 16:36:36 -0500

    Add working (sigh) starter demo

---
+++ curriculum/class-11/demo/server/src/auth/router.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+
+const User = require('./users-model.js');
+const auth = require('./middleware.js');
+
+authRouter.post('/signup', (req, res, next) => {
+  let user = new User(req.body);
+  user.save()
+    .then( (user) => {
+      req.token = user.generateToken();
+      req.user = user;
+      res.set('token', req.token);
+      res.cookie('auth', req.token);
+      res.send(req.token);
+    }).catch(next);
+});
+
+authRouter.get('/signin', auth, (req, res, next) => {
+  res.cookie('auth', req.token);
+  res.send(req.token);
+});
+
+authRouter.get('/secret', auth, (req, res) => { res.send('secret!')})
+
+module.exports = authRouter;
