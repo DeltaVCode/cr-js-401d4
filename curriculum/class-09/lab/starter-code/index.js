435ac86ab0de6dfec0513cafbf1ab16dfb77de27
Keith Dahlby <keith@newbo.co>
2019-10-02 19:19:25 -0500

    MongoClient: { useUnifiedTopology: true }

--- curriculum/class-09/lab/starter-code/index.js
+++ curriculum/class-09/lab/starter-code/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const mongooseOptions = {
   useNewUrlParser:true,
   useCreateIndex: true,
+  useUnifiedTopology: true,
 };
 mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
 
--- curriculum/class-13/lab/starter-code/auth-server/index.js
+++ curriculum/class-13/lab/starter-code/auth-server/index.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const options = {
   useNewUrlParser:true,
   useCreateIndex: true,
+  useUnifiedTopology: true,
 };
 mongoose.connect(process.env.MONGODB_URI, options);
 
