393144e86b47d4f9eb57963b5694388fb8a64a9d
Keith Dahlby <keith@newbo.co>
2019-10-08 11:24:13 -0500

    Basic socket server

---
+++ curriculum/class-17/demo/server/server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const net = require('net');
+const uuid = require('uuid');
+
+const PORT = process.env.PORT || 3001;
+const server = net.createServer();
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+const socketPool = {};
+
+server.on('connection', socket => {
+  const id = uuid();
+  socket.id = id;
+  socketPool[id] = socket;
+
+  console.log(`${id} connected! ${Object.keys(socketPool).length} total.`);
+
+  socket.on('data', dataHandler);
+  socket.on('error', err => {
+    console.error(err);
+  });
+  socket.on('close', () => {
+    console.log(`Closing time for ${id}! ${Object.keys(socketPool).length} left.`);
+    delete socketPool[id];
+  });
+});
+
+function dataHandler(buffer) {
+  console.log(this.id, buffer);
+}
