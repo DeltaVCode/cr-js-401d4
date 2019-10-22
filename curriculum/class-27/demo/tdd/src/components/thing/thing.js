1167684099baf9d02dc0acb1f32655e41922057b
Keith Dahlby <keith@newbo.co>
2019-10-22 09:50:31 -0500

    Demo 27 starter code

---
+++ curriculum/class-27/demo/tdd/src/components/thing/thing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './thing.scss';
+
+class Thing extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <section className="thing">
+      </section>
+    );
+  }
+}
+
+export default Thing;
