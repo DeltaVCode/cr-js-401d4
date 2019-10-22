21fff5fd29a9cb0fcc0d9c7352b5d97b342cefae
Keith Dahlby <keith@newbo.co>
2019-10-22 10:47:32 -0500

    TDD a toggle button!

--- curriculum/class-27/demo/tdd/src/components/thing/thing.js
+++ curriculum/class-27/demo/tdd/src/components/thing/thing.js
@@ -4,11 +4,26 @@ import './thing.scss';
 class Thing extends React.Component {
   constructor() {
     super();
+    this.state = {
+      flag: true,
+    };
+
+    // Have to do this if handleClick defined as a method
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    // Again, avoid updating state using this.state
+    // this.setState({ flag: !this.state.flag });
+
+    this.setState(state => ({ flag: !state.flag }) );
   }
 
   render() {
     return (
       <section className="thing">
+        <span className="flag">{this.state.flag ? 'True' : 'False'}</span>
+        <button onClick={this.handleClick}>Toggle!</button>
       </section>
     );
   }
---
+++ curriculum/class-27/demo/tdd/src/components/thing/thing.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Thing from './thing';
+
+describe('<Thing />', () => {
+  it('can render initially in section.thing', () => {
+    let app = shallow(<Thing />);
+    expect(app.find('section.thing').exists()).toBe(true);
+  });
+
+  it('has flag: true initially', () => {
+    let app = shallow(<Thing />);
+    expect(app.state('flag')).toBe(true);
+
+    expect(app.find('span.flag').text()).toBe('True');
+  });
+
+  it('can click button to toggle flag', () => {
+    // Arrange
+    let app = shallow(<Thing />);
+    let button = app.find('button');
+    expect(button.exists()).toBe(true);
+    expect(button.text()).toBe('Toggle!');
+
+    // Act
+    button.simulate('click');
+
+    // Assert
+    expect(app.state('flag')).toBe(false);
+    expect(app.find('span.flag').text()).toBe('False');
+
+    // Act
+    button.simulate('click');
+
+    // Assert
+    expect(app.state('flag')).toBe(true);
+    expect(app.find('span.flag').text()).toBe('True');
+  });
+});
