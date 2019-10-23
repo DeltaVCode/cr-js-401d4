18ce0b5d0602081240d261a4210e36ebfea6e68f
Keith Dahlby <keith@newbo.co>
2019-10-23 10:52:32 -0500

    Add button to look up SW data and call back into App

--- curriculum/class-28/demo/props-and-state/src/app.js
+++ curriculum/class-28/demo/props-and-state/src/app.js
@@ -8,11 +8,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      count: undefined,
+      results: [],
     };
   }
+
+  handleFormData = (count, results) => {
+    console.log('handleFormData', results);
+    this.setState({ count, results });
+  }
+
   render() {
     return (
-      <>
+      <> {/* React.Fragment */}
+        <Form
+          onDataReceived={this.handleFormData}
+          buttonText="Use the Force"
+          buttonCount={4}
+          buttonCountString="4"
+          formEnabled
+          buttonStyle={{ color: 'blue' }}
+          >
+            <strong>Star Wars API!</strong>
+            <p>This is a lot of fun.</p>
+        </Form>
         <ul>
         </ul>
       </>
--- curriculum/class-28/demo/props-and-state/src/form.js
+++ curriculum/class-28/demo/props-and-state/src/form.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import superagent from 'superagent';
 
 class Form extends React.Component {
   constructor(props) {
     super(props);
+    console.log('Form props', props);
   }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    let data = await superagent.get('https://swapi.co/api/people');
+    console.log(data);
+    let { count, results } = data.body;
+
+    if (typeof this.props.onDataReceived === 'function') {
+      this.props.onDataReceived(count, results);
+    }
+  };
 
   render() {
     return (
-      'form'
+      <form onSubmit={this.handleSubmit}>
+        <h1>Form!</h1>
+        {this.props.children}
+        <label>
+          <input type="checkbox" required />
+          Check this.
+        </label>
+        <button style={this.props.buttonStyle}>
+          {this.props.buttonText}
+        </button>
+      </form>
     );
   }
 }
