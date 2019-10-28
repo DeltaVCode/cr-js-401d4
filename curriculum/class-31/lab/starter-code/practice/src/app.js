6797573b6e860ba1979b6e1fdc3093139ce57206
Keith Dahlby <keith@newbo.co>
2019-10-28 13:38:52 -0500

    useReducer for words state

--- curriculum/class-31/lab/starter-code/practice/src/app.js
+++ curriculum/class-31/lab/starter-code/practice/src/app.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { useEffect, useState, useReducer } from 'react';
 
+const initialState = { words: '' };
+function reducer(state, action) {
+  console.log('reducer', state, action);
+  switch(action.type) {
+    case 'enter':
+      return {
+        words: action.payload.toUpperCase(),
+      };
+    case 'reset':
+      return initialState;
+    default:
+      throw action.type;
+  }
+}
+
 export default function App() {
   const [input, setInput] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <>
