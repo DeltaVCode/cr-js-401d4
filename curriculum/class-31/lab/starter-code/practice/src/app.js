7747f531a489eb0837e0613bd47a5b367c6d45ce
Keith Dahlby <keith@newbo.co>
2019-10-28 13:36:24 -0500

    useState for input

--- curriculum/class-31/lab/starter-code/practice/src/app.js
+++ curriculum/class-31/lab/starter-code/practice/src/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useEffect, useState, useReducer } from 'react';
 
 export default function App() {
+  const [input, setInput] = useState('');
+
   return (
     <>
       <div>My App</div>
