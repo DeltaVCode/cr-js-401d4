964e2a0d48e30314d2a650224cfe8ed340da1adf
Keith Dahlby <keith@newbo.co>
2019-10-28 10:53:29 -0500

    Add basic counter with useState hook

--- curriculum/class-31/demo/hooks-demo/src/hooks-counter.js
+++ curriculum/class-31/demo/hooks-demo/src/hooks-counter.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function HooksCounter() {
+export default function HooksCounter(props) {
+  // Only props are from <Route />
+  // console.log(props);
+
+  let initialCount = 0;
+  let [count, setCount] = useState(initialCount);
+  function increment() {
+    setCount(count + 1);
+  }
+
+  let initialRandom = Math.random();
+  let [random, setRandom] = useState(initialRandom);
+
   return (
     <div>
+      <h2><code>setState</code> Count: {count}</h2>
+      <button onClick={increment}>
+        Update Counter
+      </button>
+      <h3>Random {random}</h3>
+      <button onClick={() => setRandom(Math.floor(Math.random() * 100))}>
+        Generate Random Number
+      </button>
     </div>
   );
 }
