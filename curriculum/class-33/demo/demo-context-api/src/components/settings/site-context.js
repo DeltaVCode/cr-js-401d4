4ee74d25a7968abc098acbf4edd6d24bac1dbecc
Keith Dahlby <keith@newbo.co>
2019-10-30 10:59:25 -0500

    Add SettingsProvider

--- curriculum/class-33/demo/demo-context-api/src/components/settings/site-context.js
+++ curriculum/class-33/demo/demo-context-api/src/components/settings/site-context.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
+export const SettingsContext = React.createContext();
+
 class SettingsProvider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      title: 'DeltaV Context Demo',
+    };
+  }
+
+  render() {
+    return (
+      <SettingsContext.Provider value={this.state}>
+        {this.props.children}
+      </SettingsContext.Provider>
+    );
+  }
 }
 
 export default SettingsProvider;
