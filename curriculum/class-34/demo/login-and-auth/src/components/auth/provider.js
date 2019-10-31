4b5cd4e59820cbe859790523e6944da827caf693
Keith Dahlby <keith@newbo.co>
2019-10-31 10:56:14 -0500

    Actually get token from username/password

--- curriculum/class-34/demo/login-and-auth/src/components/auth/provider.js
+++ curriculum/class-34/demo/login-and-auth/src/components/auth/provider.js
@@ -1,18 +1,51 @@
 import React from 'react';
+import jwt from 'jsonwebtoken';
 
 import LoginContext from './context';
+
+const API = process.env.REACT_APP_API;
 
 export default class LoginProvider extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      token: null,
+      user: null,
       login: this.login,
     };
   }
 
   login = (username, password) => {
     console.log('Login!', username);
+
+    fetch(`${API}/signin`, {
+      mode: 'cors',
+      method: 'post',
+      cache: 'no-cache',
+      headers: new Headers({
+        'Authorization': `Basic ${btoa(`${username}:${password}`)}`,
+      })
+    })
+      .then(response => response.text())
+      .then(token => this.validateToken(token))
+      .catch(console.error);
+  }
+
+  validateToken = token => {
+    try {
+      let user = jwt.decode(token);
+      console.log(user);
+      this.setLoginState(token, user);
+    }
+    catch(e) {
+      this.setLoginState(null, null);
+      console.error(e);
+    }
+  }
+
+  setLoginState = (token, user) => {
+    this.setState({ token, user });
   }
 
   render() {
