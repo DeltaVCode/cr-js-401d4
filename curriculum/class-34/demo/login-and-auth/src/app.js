41e0b5b6c3ca04f3a385047a0fbc38b8cdd91aef
Keith Dahlby <keith@newbo.co>
2019-10-31 11:23:02 -0500

    Add Auth component

--- curriculum/class-34/demo/login-and-auth/src/app.js
+++ curriculum/class-34/demo/login-and-auth/src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import LoginProvider from './components/auth/provider';
 import Login from './components/auth/login';
+import Auth from './components/auth/auth';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,24 @@ class App extends React.Component {
         <header>
           <Login />
         </header>
+        <ul>
+          <li>Anyone can see this</li>
+          <Auth>
+            <li>Authed users can see this</li>
+          </Auth>
+          <Auth capability="read">
+            <li>Readers can see this</li>
+          </Auth>
+          <Auth capability="create">
+            <li>Creators can see this</li>
+          </Auth>
+          <Auth capability="update">
+            <li>Updaters can see this</li>
+          </Auth>
+          <Auth capability="delete">
+            <li>Deleters can see this</li>
+          </Auth>
+        </ul>
       </LoginProvider>
     );
   }
--- curriculum/class-34/demo/login-and-auth/src/components/auth/auth.js
+++ curriculum/class-34/demo/login-and-auth/src/components/auth/auth.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
+import LoginContext from './context';
+
 class Auth extends React.Component {
+  static contextType = LoginContext;
+
+  render() {
+    let { user } = this.context;
+    let { capability } = this.props;
+
+    if (!user)
+      return null;
+
+    if (capability &&
+      !(user.capabilities && user.capabilities.includes(capability)))
+      return null;
+
+    return this.props.children;
+  }
 }
 
 export default Auth;
