87c1cf4872587e3e27e8df851a6ee3e96cc4e2c6
Keith Dahlby <keith@newbo.co>
2019-11-01 13:15:00 -0500

    Add basic request history

--- curriculum/class-28/demo/resty/src/components/resty/index.js
+++ curriculum/class-28/demo/resty/src/components/resty/index.js
@@ -1,22 +1,40 @@
 import './resty.scss';
 
 import React, { useState } from 'react';
-import superagent from 'superagent';
 import ReactJson from 'react-json-view';
 
 import Form from './form';
 import makeRequest from './api';
 
+function History(props) {
+  return (
+    <ul>
+      {props.history.map(h => (
+        <li>{h.method.toUpperCase()} {h.url}</li>
+      ))}
+    </ul>
+  )
+}
+
 function RESTy() {
   let [response, setResponse] = useState({});
+  let [history, setHistory] = useState([]);
 
   let callAPI = (method, url, body) => {
+    setHistory(h => [
+      {method, url, body},
+      ...h,
+    ]);
+
     makeRequest(method, url, body)
       .then(setResponse);
   };
 
   return (
     <main>
+      <aside>
+        <History history={history} />
+      </aside>
       <section className="deck">
         <Form
           onRequest={callAPI}
