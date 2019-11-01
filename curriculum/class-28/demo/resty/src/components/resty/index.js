cf7744e1e1291260b93d94d64416d22f0066efef
Keith Dahlby <keith@newbo.co>
2019-11-01 10:55:37 -0500

    Refactor RESTy as function component

--- curriculum/class-28/demo/resty/src/components/resty/index.js
+++ curriculum/class-28/demo/resty/src/components/resty/index.js
@@ -1,63 +1,57 @@
 import './resty.scss';
 
-import React from 'react';
+import React, { useState } from 'react';
 import superagent from 'superagent';
 import ReactJson from 'react-json-view';
 
 import Form from './form';
 
-class RESTy extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      header: {},
-      body: {},
-      history: {},
-      headersVisible: false,
-    };
-  }
+function RESTy() {
+  let [response, setResponse] = useState({});
 
-  callAPI = (method, url, body) => {
+  let callAPI = (method, url, body) => {
     superagent(method, url)
       .set('Content-Type', 'application/json')
       .send(body)
       .then(response => {
         let header = response.header;
         let body = response.body;
-        this.setState({ header, body });
+        setResponse({
+          header, body,
+        });
       })
       .catch(e => {
         let body = { error: e.message };
         let header = {};
-        this.setState({ header, body });
+        setResponse({
+          header, body,
+        });
       });
   };
 
-  render() {
-    return (
-      <main>
-        <section className="deck">
-          <Form
-            onRequest={this.callAPI}
-            />
-          <div id="json">
-            <ReactJson
-              name="Headers"
-              enableClipboard={false}
-              collapsed={true}
-              src={this.state.header}
-            />
-            <ReactJson
-              name="Response"
-              enableClipboard={false}
-              collapsed={false}
-              src={this.state.body}
-            />
-          </div>
-        </section>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <section className="deck">
+        <Form
+          onRequest={callAPI}
+          />
+        <div id="json">
+          <ReactJson
+            name="Headers"
+            enableClipboard={false}
+            collapsed={true}
+            src={response.header}
+          />
+          <ReactJson
+            name="Response"
+            enableClipboard={false}
+            collapsed={false}
+            src={response.body}
+          />
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default RESTy;
