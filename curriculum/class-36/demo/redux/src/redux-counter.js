76300647ce35495ca04c2025ac69dfe1ecb2a9a6
Keith Dahlby <keith@newbo.co>
2019-11-04 11:48:26 -0600

    Refactor a little bit

--- curriculum/class-36/demo/redux/src/redux-counter.js
+++ curriculum/class-36/demo/redux/src/redux-counter.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { add, increment, decrement, reset } from './store/counter-reducer';
 
 function ReduxCounter(props) {
   console.log(props);
-  const { count, increment, dispatch } = props;
+  const { count, increment, decrement, reset, add7 } = props;
 
   return (
     <div>
       <h2>Redux Count: {count}</h2>
+      <button onClick={add7}>
+        +7
+      </button>
       <button onClick={increment}>
         Increment Counter
       </button>
-      <button onClick={() => dispatch({ type: 'decrement' })}>
+      <button onClick={decrement}>
         Decrement Counter
       </button>
-      <button onClick={() => dispatch({ type: 'reset' })}>
+      <button onClick={reset}>
         Reset Counter
       </button>
     </div>
@@ -30,8 +34,10 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch,
-    increment: () => dispatch({ type: 'increment' }),
+    increment: () => dispatch(increment()),
+    add7: () => dispatch(add(7)),
+    decrement: () => dispatch(decrement()),
+    reset: () => dispatch(reset()),
   };
 }
 
