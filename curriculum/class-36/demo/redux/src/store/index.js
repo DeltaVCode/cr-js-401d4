82bfcfe37a26d2c202a94aaad5aae44af61c8869
Keith Dahlby <keith@newbo.co>
2019-11-04 12:00:32 -0600

    Enable Redux dev tools

--- curriculum/class-36/demo/redux/src/store/index.js
+++ curriculum/class-36/demo/redux/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { reducer as counter } from './counter-reducer';
 import { reducer as name } from './name-reducer';
@@ -11,5 +12,5 @@ let reducer = combineReducers({
 console.log('Initial combined reducers', reducer());
 
 export default function() {
-  return createStore(reducer);
+  return createStore(reducer, composeWithDevTools());
 }
