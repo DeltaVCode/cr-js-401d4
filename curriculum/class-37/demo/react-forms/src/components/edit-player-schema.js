e53271a2a651af8da9be4d45bf8666945fc948d5
Keith Dahlby <keith@newbo.co>
2019-11-05 11:12:52 -0600

    Reformat

--- curriculum/class-37/demo/react-forms/src/components/edit-player-schema.js
+++ curriculum/class-37/demo/react-forms/src/components/edit-player-schema.js
@@ -3,13 +3,25 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const EditPlayerSchema = (props) => {
+  let player = props.player; // props.players[props.match.params.id];
+  if (!player) {
+    return (
+      <strong>Player {props.match.params.id} not found!</strong>
+    );
+  }
+
   return (
     <form>
-      Editing {props.match.params.id}
+      <h2>Editing {player.name}</h2>
       <p><Link to="/">Go Home</Link></p>
     </form>
   );
 };
 
 export default connect(
+  (state, props) => ({
+    // players: state.players, // We don't need all the players, just one
+
+    player: state.players[props.match.params.id],
+  })
 )(EditPlayerSchema);
