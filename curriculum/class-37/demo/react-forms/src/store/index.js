318aad624a80d01a9a2a35a82b2d213371ed0a00
Keith Dahlby <keith@newbo.co>
2019-11-05 10:44:11 -0600

    Add players reducer + actions

--- curriculum/class-37/demo/react-forms/src/store/index.js
+++ curriculum/class-37/demo/react-forms/src/store/index.js
@@ -1,7 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import players from './players-reducer';
+
 let reducers = combineReducers({
+  players,
 });
 
 export default () => createStore(reducers, composeWithDevTools());
