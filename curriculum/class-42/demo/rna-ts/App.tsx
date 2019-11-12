afc334022801eaa46c6d46ed0aa1c5968c86f245
Keith Dahlby <keith@newbo.co>
2019-11-12 11:09:24 -0600

    Bit of TypeScript demo

--- curriculum/class-42/demo/rna-ts/App.tsx
+++ curriculum/class-42/demo/rna-ts/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Button, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  let [num, setNum] = useState(42);
+
   return (
     <View style={styles.container}>
-      <Text>React Native with TypeScript</Text>
+      <TestComponent name='Keith' dob={new Date(2000,1,1)} />
+      <Text>Number: {num}</Text>
+      <TextInput onChangeText={text => setNum(parseInt(text, 10))} />
+      <Button onPress={() => setNum(5)} title="Set Number" />
     </View>
+  );
+}
+
+function TestComponent(props: { name : string, dob?: Date }) {
+  return (
+    <>
+      <Text>My name is {props.name}.</Text>
+      {props.dob ? <Text>My age is {new Date().getFullYear() - props.dob.getFullYear()}.</Text> : <Text>Age unknown</Text>}
+    </>
   );
 }
 
